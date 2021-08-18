d58007732aaabfd155d49d263c0308fb8d7d86b8
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-17 22:19:54 -0500

    Create helper factory method

--- RandomUserTests/ProfileUserLoaderTests.swift
+++ RandomUserTests/ProfileUserLoaderTests.swift
@@ -24,19 +24,28 @@ final class ProfileUserLoader {
 
 class ProfileUserLoaderTests: XCTestCase {
   func test_init_doesNotRequestURL() {
-    let client = HTTPClientSpy()
-    let _ = ProfileUserLoader(url: URL(string: "http://any-url.com")!, client: client)
+    let (_, client) = makeSUT()
     
     XCTAssertEqual(client.requestedURLs, [], "Client should not request url on init")
   }
   
   func test_load_requestsDataFromURL() {
     let url = URL(string: "http://any-url.com")!
-    let client = HTTPClientSpy()
-    let sut = ProfileUserLoader(url: url, client: client)
+    let (sut, client) = makeSUT(with: url)
     
     sut.load()
     
     XCTAssertEqual(client.requestedURLs, [url])
+  }
+  
+  // MARK: - Helpers
+  
+  private func makeSUT(with url: URL = URL(string: "http://any-url.com")!, 
+                       file: StaticString = #file, 
+                       line: UInt = #line) -> (sut: ProfileUserLoader, client: HTTPClientSpy) {
+    let client = HTTPClientSpy()
+    let sut = ProfileUserLoader(url: url, client: client)
+    
+    return (sut, client)
   }
 }
