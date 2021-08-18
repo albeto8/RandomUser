cdb1aee72a0b05b3a0278a70a932cc5a4eee6f19
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-17 22:17:30 -0500

    tProfileUserLoader requests data from URL

--- RandomUserTests/ProfileUserLoaderTests.swift
+++ RandomUserTests/ProfileUserLoaderTests.swift
@@ -9,18 +9,34 @@ import XCTest
 import RandomUser
 
 final class ProfileUserLoader {
+  private let url: URL
   private let client: HTTPClient
   
-  init(client: HTTPClient) {
+  init(url: URL, client: HTTPClient) {
+    self.url = url
     self.client = client
+  }
+  
+  func load() {
+    client.get(from: url) { _ in }
   }
 }
 
 class ProfileUserLoaderTests: XCTestCase {
   func test_init_doesNotRequestURL() {
     let client = HTTPClientSpy()
-    let _ = ProfileUserLoader(client: client)
+    let _ = ProfileUserLoader(url: URL(string: "http://any-url.com")!, client: client)
     
     XCTAssertEqual(client.requestedURLs, [], "Client should not request url on init")
+  }
+  
+  func test_load_requestsDataFromURL() {
+    let url = URL(string: "http://any-url.com")!
+    let client = HTTPClientSpy()
+    let sut = ProfileUserLoader(url: url, client: client)
+    
+    sut.load()
+    
+    XCTAssertEqual(client.requestedURLs, [url])
   }
 }
