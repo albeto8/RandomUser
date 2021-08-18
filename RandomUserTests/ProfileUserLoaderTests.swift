f3b95a939cc1dd342f723e44ca54d1e2de700488
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-17 22:36:58 -0500

    ProfileUserLoader deliversErrorOnClientError

--- RandomUserTests/ProfileUserLoaderTests.swift
+++ RandomUserTests/ProfileUserLoaderTests.swift
@@ -12,13 +12,23 @@ final class ProfileUserLoader {
   private let url: URL
   private let client: HTTPClient
   
+  typealias Result = Swift.Result<User, Swift.Error>
+  
   init(url: URL, client: HTTPClient) {
     self.url = url
     self.client = client
   }
   
-  func load() {
-    client.get(from: url) { _ in }
+  func load(completion: @escaping (Result) -> Void) {
+    client.get(from: url) { result in
+      switch result {
+      case .failure(let error):
+        completion(.failure(error))
+        
+      default:
+        break
+      }
+    }
   }
 }
 
@@ -33,9 +43,30 @@ class ProfileUserLoaderTests: XCTestCase {
     let url = URL(string: "http://any-url.com")!
     let (sut, client) = makeSUT(with: url)
     
-    sut.load()
+    sut.load { _ in }
     
     XCTAssertEqual(client.requestedURLs, [url])
+  }
+  
+  func test_load_deliversErrorOnClientError() {
+    let (sut, client) = makeSUT()
+    let clientError = NSError(domain: "anyError", code: 0)
+    let exp = expectation(description: "Wait for completion")
+        
+    sut.load { receivedResult in
+      switch receivedResult {
+      case .failure(let error as NSError):
+        XCTAssertEqual(error, clientError)
+        
+      default:
+        XCTFail("Expected result \(clientError) got \(receivedResult) instead")
+      }
+      exp.fulfill()
+    }
+    
+    client.complete(with: clientError)
+    
+    wait(for: [exp], timeout: 0.1)
   }
   
   // MARK: - Helpers
