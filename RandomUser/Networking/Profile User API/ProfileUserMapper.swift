7aaf52f85217d4e467835ebba644f208671a4f7e
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-17 22:02:04 -0500

    Move ProfileUserMapper to production

---
+++ RandomUser/Networking/Profile User API/ProfileUserMapper.swift
@@ -0,0 +1,99 @@
+//
+//  ProfileUserMapper.swift
+//  RandomUser
+//
+//  Created by Mario Alberto Barragán Espinosa on 17/08/21.
+//
+
+import Foundation
+
+public final class ProfileUserMapper {  
+  private struct Root: Decodable {
+    private let results: RemoteUser
+    
+    private struct RemoteUser: Decodable {
+      private let gender: String
+      private let name: RemoteName
+      private let location: RemoteLocation
+      private let email: String
+      private let dob: RemoteDob
+      private let registered: RemoteRegistered
+      private let phone, cell: String
+      private let picture: RemotePicture
+      
+      private struct RemoteName: Decodable {
+        let title: String
+        let first: String
+        let last: String
+      }
+      
+      private struct RemoteDob: Decodable {
+        let date: String
+        let age: Int
+      }
+      
+      private struct RemoteRegistered: Decodable {
+        let date: String
+      }
+      
+      private struct RemoteLocation: Decodable {
+        let street: RemoteStreet
+        let city: String
+        let state: String
+        let country: String
+        let postcode: String
+        let coordinates: RemoteCoordinates
+      }
+      
+      private struct RemoteCoordinates: Decodable {
+        let latitude: String
+        let longitude: String
+      }
+      
+      private struct RemoteStreet: Decodable {
+        let number: Int
+        let name: String
+      }
+      
+      private struct RemotePicture: Decodable {
+        let large: String
+      }
+      
+      var user: User {
+        return User(nameTitle: name.title, 
+                    firstName: name.first, 
+                    lastName: name.last, 
+                    gender: gender, 
+                    email: email, 
+                    birthDay: dob.date, 
+                    age: dob.age, 
+                    phone: phone, 
+                    cellPhone: cell, 
+                    userPictureURL: URL(string: picture.large), 
+                    registrationDate: registered.date, 
+                    address: Address(streetNumber: location.street.number, 
+                                     streetName: location.street.name, 
+                                     city: location.city, 
+                                     state: location.state, 
+                                     country: location.country, 
+                                     latitude: location.coordinates.latitude, 
+                                     longitude: location.coordinates.longitude, 
+                                     postcode: location.postcode))
+      }
+    }
+    
+    var dto: User {
+      return results.user
+    }
+  }
+  
+  public static func map(data: Data) throws -> User {
+    do {
+      let root = try JSONDecoder().decode(Root.self, from: data)
+      return root.dto
+      
+    } catch {
+      throw error
+    }    
+  }
+}
---
+++ RandomUser/Networking/Profile User Feature/User.swift
@@ -0,0 +1,60 @@
+//
+//  User.swift
+//  RandomUser
+//
+//  Created by Mario Alberto Barragán Espinosa on 17/08/21.
+//
+
+import Foundation
+
+public struct User: Equatable {
+  let nameTitle: String
+  let firstName: String
+  let lastName: String
+  let gender: String
+  let email: String
+  let birthDay: String
+  let age: Int
+  let phone: String
+  let cellPhone: String
+  let userPictureURL: URL?
+  let registrationDate: String 
+  let address: Address
+  
+  public init(nameTitle: String, firstName: String, lastName: String, gender: String, email: String, birthDay: String, age: Int, phone: String, cellPhone: String, userPictureURL: URL?, registrationDate: String, address: Address) {
+    self.nameTitle = nameTitle
+    self.firstName = firstName
+    self.lastName = lastName
+    self.gender = gender
+    self.email = email
+    self.birthDay = birthDay
+    self.age = age
+    self.phone = phone
+    self.cellPhone = cellPhone
+    self.userPictureURL = userPictureURL
+    self.registrationDate = registrationDate
+    self.address = address
+  }
+}
+
+public struct Address: Equatable {
+  let streetNumber: Int
+  let streetName: String
+  let city: String
+  let state: String
+  let country: String
+  let latitude: String
+  let longitude: String
+  let postcode: String
+  
+  public init(streetNumber: Int, streetName: String, city: String, state: String, country: String, latitude: String, longitude: String, postcode: String) {
+    self.streetNumber = streetNumber
+    self.streetName = streetName
+    self.city = city
+    self.state = state
+    self.country = country
+    self.latitude = latitude
+    self.longitude = longitude
+    self.postcode = postcode
+  }
+}
