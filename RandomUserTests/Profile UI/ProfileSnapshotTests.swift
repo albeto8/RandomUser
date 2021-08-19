b08b5419b53fb5700407fcf20edeff56f22edc9e
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-19 13:13:02 -0500

    Fix snapshot tests

--- RandomUserTests/Profile UI/ProfileSnapshotTests.swift
+++ RandomUserTests/Profile UI/ProfileSnapshotTests.swift
@@ -10,22 +10,42 @@ import XCTest
 
 class ProfileSnapshotTests: XCTestCase {
   func test_profileWithContent() {
-    let profileViewModel = ProfileViewModel(loader: UserLoaderDummy())
-    let sut = ProfileViewController(profileViewModel: profileViewModel)
+    let sut = makeSUT()
     
-    sut.loadViewIfNeeded()
-    sut.display(UserInfoViewModel.prototype)
-    
+    sut.display(userImage: UIImage(named: "randomUser1")!)
+        
     assert(sut, mode: .light)
     assert(sut, mode: .dark)
   }
   
+  // MARK: - Helpers
+  
+  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> ProfileViewController {
+    let sut = ProfileUIComposer.controllerWith(userLoader: UserLoaderDummy(), imageLoader: UserImageDataLoaderDummy())
+    
+    sut.loadViewIfNeeded()
+    
+    return sut
+  }
+  
   private class UserLoaderDummy: UserLoader {
-    func load(completion: @escaping (UserLoader.Result) -> Void) {}
+    func load(completion: @escaping (UserLoader.Result) -> Void) {
+      completion(.success(User.prototypeUser))
+    }
+  }
+  
+  private class UserImageDataLoaderDummy: UserImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (UserImageDataLoader.Result) -> Void) -> UserImageDataLoaderTask {      
+      return UserImageDataLoaderTaskDummy()
+    }
+  }
+  
+  private struct UserImageDataLoaderTaskDummy: UserImageDataLoaderTask {
+    func cancel() {}
   }
 }
 
-extension UserInfoViewModel {
+extension User {
   
   static let prototypeUser = makeUserItem(nameTitle: "Mr", 
                                           firstName: "Maxime", 
@@ -45,7 +65,18 @@ extension UserInfoViewModel {
                                           country: "Canada", 
                                           latitude: "-68.1548", 
                                           longitude: "-73.3002", 
-                                          postcode: "B9Y 6D8").model
-  
-  static let prototype = UserInfoViewModel(user: UserInfoViewModel.prototypeUser)
+                                          postcode: "B9Y 6D8").model  
+}
+
+extension UIImage {
+  static func make(withColor color: UIColor) -> UIImage {
+    let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+    UIGraphicsBeginImageContext(rect.size)
+    let context = UIGraphicsGetCurrentContext()!
+    context.setFillColor(color.cgColor)
+    context.fill(rect)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return img!
+  }
 }
